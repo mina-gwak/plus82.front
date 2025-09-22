865e43d12b097663affda2d763387d3a71f17aa1
mina-gwak <mina.gwak@kakao.com>
2025-09-22 13:18:31 +0000

    Fix: PD-346 줄바꿈 추가

--- src/entities/job-post/ui/posting-title/index.tsx
+++ src/entities/job-post/ui/posting-title/index.tsx
@@ -1,13 +1,13 @@
-import { format } from 'date-fns'
+import { differenceInDays, format } from 'date-fns'
 import { lowerCase } from 'lodash-es'
 import { useTranslations } from 'next-intl'
 
 import { colors } from 'shared/config'
-import { cn } from 'shared/lib'
+import { cn, isNilOrEmptyString } from 'shared/lib'
 import { Icon } from 'shared/ui'
 
 import * as css from './variants'
-import { convertStudentType } from '../../model/convert-to-student-type'
+import { convertStudentTypeToArray } from '../../model/convert-to-student-type'
 import { JobPost } from '../../model/job-post'
 
 type Props = {
@@ -18,13 +18,16 @@ type Props = {
 export const PostingTitle = ({ jobPost, size }: Props) => {
   const t = useTranslations()
 
-  const studentType = convertStudentType({
+  const studentType = convertStudentTypeToArray({
     forKindergarten: jobPost.forKindergarten,
     forElementary: jobPost.forElementary,
     forMiddleSchool: jobPost.forMiddleSchool,
     forHighSchool: jobPost.forHighSchool,
     forAdult: jobPost.forAdult,
   })
+
+  const hasMultipleStudentType = studentType.length > 1
+  const hasAllStudentType = studentType.length === 5
 
   return (
     <div className={cn(css.container({ size }))}>
@@ -41,14 +44,46 @@ export const PostingTitle = ({ jobPost, size }: Props) => {
         </li>
         <li className={cn(css.description({ size }))}>
           <Icon name="User" color={colors.gray[500]} size={size} />
-          <span>{studentType ?? '-'}</span>
+          <span>
+            {(() => {
+              if (hasAllStudentType) {
+                return 'All'
+              }
+
+              if (hasMultipleStudentType) {
+                return `${studentType[0]} and Others`
+              }
+
+              return studentType[0]
+            })()}
+          </span>
         </li>
         <li className={cn(css.description({ size }))}>
           <Icon name="Date" color={colors.gray[500]} size={size} />
           <span>
-            {jobPost.dueDate
-              ? `~${format(jobPost.dueDate, 'yyyy.MM.dd')}`
-              : '-'}
+            {(() => {
+              if (isNilOrEmptyString(jobPost.dueDate)) {
+                return 'Ongoing Recruitment'
+              }
+
+              const dueDate = new Date(jobPost.dueDate).setHours(0, 0, 0, 0)
+              const today = new Date().setHours(0, 0, 0, 0)
+              const diffDays = differenceInDays(dueDate, today)
+
+              if (diffDays >= 8) {
+                return `~${format(dueDate, 'yyyy.MM.dd')}`
+              }
+
+              if (diffDays <= 7 && diffDays > 0) {
+                return `D-${diffDays}`
+              }
+
+              if (diffDays === 0) {
+                return 'Ends Today'
+              }
+
+              return 'Closed'
+            })()}
           </span>
         </li>
       </ul>
